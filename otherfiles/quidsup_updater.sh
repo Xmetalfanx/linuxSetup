80adbb8c9166cafbd06a5e5ecd70677edcccb27a
Xmetalfanx <Xmetalfanx@yahoo.com>
2016-12-22 19:31:12 -0500

    Distro Detection and conlidation work
    Work on cobining mulitple functions into one

--- otherfiles/quidsup_updater.sh
+++ otherfiles/quidsup_updater.sh
@@ -1,10 +1,14 @@
-echo "Updating System"
+#!/bin/bash
+#Name: Package Manager Tutorial
+#Description: Script checks which package manager is being used, and then opens the appropriate function
+#  Fill out the list of packages you require.
+#  Note: Package names can differ between Linux Distros
+#Video:
 
----
-Update_Ubuntu() {
+Install_Apt() {
   #Ubuntu
   echo "Preparing to Install Deb Packages with Apt..."
-  sudo apt update
+  sudo apt -y install
 }
 Install_AptGet() {
   #Debian, Ubuntu
@@ -16,17 +20,23 @@ Install_Aptitude() {
   echo "Preparing to Install Deb Packages with Aptitude..."
   sudo aptitude -y install
 }
-Update_RPM() {
+Install_Dnf() {
   #Redhat, Fedora
-  sudo dnf update
+  echo "Preparing to Install RPM packages using Dnf..."
+  sudo dnf -y install
 }
-Update_Arch() {
+Install_Pacman() {
   #Arch
-  sudo pacman -Syyu
+  echo "Preparing to Install Arch Packages..."
+  sudo pacman -S --noconfirm
 }
-
+Install_Slackpkg() {
+  #Slackware
+  echo "Preparing to Install Slackware Packages..."
+  slackpkg install
+}
 Install_Yum() {
-  #Older versions of Redhat, Fedora, CentOS
+  #Older versions of Redhat, Fedora
   echo "Preparing to Install RPM packages using Yum..."
   sudo yum -y install
 }
@@ -36,31 +46,22 @@ Install_Zypper() {
   sudo zypper install
 }
 
-
-
-
----
-echo "Updating Ubuntu 16.04 based systems "
-sudo apt update
-clear
-sudo apt upgrade -y
-
-
-echo "Updating Ubuntu-based system prior to 16.04 "
-sudo apt-get update
-clear
-sudo apt-get upgrade -y
-
-echo "Updating Arch based systems as well as optimizing mirrors"
-sudo pacman-mirrors -g
-echo "Mirrors Optimized .. Now Updating System"
-sudo pacman -Syyu
-
-echo "Updating Fedora 23 and higher"
-sudo dnf update
-
-
-echo "Updating OpenSuse based System "
-sudo zypper up
-
-echo "Update Done "
+#Find out which package manager is in use
+if [ "$(command -v apt)" ]; then Install_Apt
+elif [ "$(command -v apt-get)" ]; then Install_AptGet
+elif [ "$(command -v aptitude)" ]; then Install_Aptitude
+elif [ "$(command -v dnf)" ]; then Install_Dnf
+elif [ "$(command -v pacman)" ]; then Install_Pacman
+elif [ "$(command -v slackpkg)" ]; then Install_Slackpkg
+elif [ "$(command -v yum)" ]; then Install_Yum
+elif [ "$(command -v zypper)" ]; then Install_Zypper
+else
+  echo "Unknown package manager"
+  echo "Install the following packages:"
+  echo -e "\tpackage1"
+  echo -e "\tpackage2"
+  echo
+  echo -en "Press any key to continue... "
+  read -rn1
+  #exit 1
+fi
