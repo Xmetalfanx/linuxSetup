1376a0325d5c1377ffc8ca1dad6a7d38cf0e0658
Xmetalfanx <Xmetalfanx@yahoo.com>
2017-12-05 14:50:43 -0500

    fixed signing script

---
+++ resignDevelopmentCommit.sh
@@ -0,0 +1,8 @@
+## First try at signing .. this i will leave here but may be 100% wrong, code-wise
+#git filter-branch --commit-filter 'git commit-tree -S "$@";' -- --all
+
+
+
+git filter-branch --commit-filter 'git commit-tree -S "$@";' HEAD
+
+
