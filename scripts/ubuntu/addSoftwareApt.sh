6a12eb76a6f3aa96c97d473bceafa192d2845166
Xmetalfanx <Xmetalfanx@yahoo.com>
2018-01-14 19:22:52 -0500

    Work on Software Section(s) and Ubuntu Section

--- scripts/ubuntu/addSoftwareApt.sh
+++ scripts/ubuntu/addSoftwareApt.sh
@@ -23,13 +23,9 @@ echo -e "Please Select Task\n"
 
 	echo -e "1. \t Install Multimedia Programs"
 	echo -e "2. \t Install Office Suite [Sub-menu]"
-	echo -e "3. \t [MAYBE MOVED TO UNIVERSAL SOFTWARE SECTION]Install Web Browser [Submenu] (Chrome, Chromium, Firefox, or Vivaldi)"
-	echo -e "4. \t Install Various Useful Apps "
-	echo -e "5. \t [MAYBE MOVED TO UNIVERSAL SOFTWARE SECTION] Install Etcher to put ISOs on USB drives"
-  	echo -e "6. \t [MAYBE MOVED TO UNIVERSAL SOFTWARE SECTION]Install Atom Editor"
-	echo -e "7. \t [MAYBE MOVED TO UNIVERSAL SOFTWARE SECTION]Install\Update MPV from PPA"
-	echo -e "8. \t [MAYBE MOVED TO UNIVERSAL SOFTWARE SECTION]Install Wine and PlayOnLinux"
-	echo -e "9. \t [MAYBE MOVED TO UNIVERSAL SOFTWARE SECTION]Install Sublime Text"
+	echo -e "3. \t Install\Update MPV from PPA"
+	echo -e "\t Install GetDebs Game PPA for Game downloads"
+	echo -e "\t Install My picks of PPA for software or the LATEST versions of certain softwares"
 
 
 	echo -e "m. \t Return to Main Menu"
@@ -45,49 +41,13 @@ echo -e "Please Select Task\n"
 			x=1
 			;;
 
-		3) browserMenu
-			x=1
-			;;
-
-		4)  echo -e "Installing Common (Various). "
-			clear
-			echo -e "About to install Filezilla, Deluge, Gparted, Bleachbit, and Etcher"
-			userPrompt
-
- 		  	aptUpdate
-
-			## ? Include QBittorrent too?
- 			installApp filezilla deluge gparted bleachbit plank radiotray gpodder pinta htop
-
-	        # Function call
-	        #etcherInstall
-			x=1
-		 	;;
-
-		5) 	# in Universal 
-			etcherInstall
-			x=1
-			;;
-
-	    6) installAtom
-				x=1
-	 	 		;;
-
-		7) updatedPPAMPV
+		3) updatedPPAMPV
 			x=1
  	 		;;
 
-		8) wineInstall
-			x=1
- 	 		;;
+		[mM]) bash "$rootdir"/xmetalLinuxScripts.sh ;;
 
-		9) sublimetextInstall
-			x=1
-			;;
-
-		[mM])	bash "$rootdir"/xmetalLinuxScripts.sh ;;
-
-  	[qQ]) quitScript	;;
+  		[qQ]) quitScript	;;
 
 		* ) invalidSection ;;
 
