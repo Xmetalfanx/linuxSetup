a79f4580d9a23a8fd950445a6788af2fccb86ab4
Xmetalfanx <Xmetalfanx@yahoo.com>
2018-02-17 00:31:44 -0500

    Various Tasks
    - Edited some test items
    
    - Adjusted Sumblime install function(s)
    
    Removed duplicate function that shouldn't have existed
    
    Testing the Universal function to install Sublime-Text
    
    Removed reference to a config file that no longer exists
    
    - This that was causing the scripts to pause/stop on first run
    
    Reorganized CFG files to hopefully prevent future goofups
    
    - Other Various Tasks

--- linuxSetupScripts.sh
+++ linuxSetupScripts.sh
@@ -11,41 +11,35 @@ export basedir
 rootdir=$basedir
 functionsdir="$rootdir"/tasks/functions
 
-distroCheck= . $functionsdir/f_distroCheck.cfg
-$distroCheck
-
 commonFunctions= . $functionsdir/f_common.cfg
 $commonFunctions
 
 universalFunctions= . $functionsdir/includeUniversalFunctions.cfg
 $universalFunctions
 
-
 ########################################################################
 x=1
 
 #Clears Screen
-clear 
+clear
 
-# Gets info about distro 
+# Gets info about distro
 # This is located in the f_distoCheck.cfg file b
 completeDistroCheck
 
-# Displays some guesses/info about the distro pre-menus 
+# Displays some guesses/info about the distro pre-menus
 mainDisplayDistroInfo
 
 
 while [ $x=1 ]; do
 
-    #echo -e "test only: $ubuntuName"
-    
     echo -e "${bold}Xmetal's Linux Script ${normal} \n"
 
     echo -e "Please Select your Distrobution family \n"
 
-    echo -e   "1. \t Update your System" 
-    echo -e   "2. \t Install Software"     
-    
+    echo -e   "1. \t Update your System"
+    echo -e   "2. \t Install Software"
+
     echo -e   "3. \t Arch-based"
     echo -e   "4. \t Fedora"
     echo -e   "5. \t OpenSUSE"
@@ -56,17 +50,17 @@ while [ $x=1 ]; do
 
 
     echo -e   "t. \t Test - Tiny Media Manager install"
-   
+
     echo -e   "q. \t Exit to Prompt \n\n"
     read -p   "Your Choice?:" mainSelection
 
     case $mainSelection in
 
       1) universalUpgrade ;;
-      
+
         ## Located in /linuxSetup/tasks/functions/universal/menus/f_universal_menus.cfg
-      2) universalSoftwareFunction ;; 
-      
+      2) universalSoftwareFunction ;;
+
       3) bash  "$scriptsDir"/archLinuxScripts.sh ;;
       4) bash  "$scriptsDir"/fedoraScripts.sh  ;;
       5) bash  "$scriptsDir"/openSuseScripts.sh  ;;
@@ -76,9 +70,8 @@ while [ $x=1 ]; do
 
       9) getPlasmaInfo $plasmaDEVersion $plasmaFrameworkVersion;;
 
-
-      ## Located in universal software config file 
-      t) tinyMediaManagerInstall ;; 
+      ## Located in universal software config file
+      t) tinyMediaManagerInstall ;;
 
 
       [qQ]) quitScript  ;;
