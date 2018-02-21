6f5a519fe349a7a09b9bd15703b7a9b130f3654d
Xmetalfanx <Xmetalfanx@yahoo.com>
2018-02-20 19:54:03 -0500

    Redid the main menus and function for Solus
    Redid a few things to make more sense and clean things up, in my view ...

--- scripts/solusScripts.sh
+++ scripts/solusScripts.sh
@@ -2,37 +2,44 @@
 
 rootdir=$basedir
 
-commonFunctions= . "$rootdir"/tasks/functions/f_common.cfg
-$commonFunctions
+
 #######################################################################################
-function solusUpdate
+
+solusUnstableRepo="https://packages.solus-project.com/unstable/eopkg-index.xml.xz"
+
+function enableUnstableRepo
 {
-   echo -e "About to Update Solus via command line, NOT TYPICALLY recommended by Solus Team"
-	 read -p "Do you want to continue? (Press any key for YES, Control + Z for NO )"
-	 sudo eopkg up
+   	# Disable "Default"/"Stable" Repo
+   	sudo eopkg disable-repo Stable 
+
+   	echo -e "Enabling UNSTABLE Solus Repo"
+   	userPrompt
+ 	sudo eopkg add-repo SolusUnstable $solusUnstableRepo
+ 	sudo eopkg enable-repo SolusUnstable
+
+
+	clear
+	echo -e "Unstable Solus repo added and enabled"
+	echo -e "*** IMPORTANT **** "
+	echo -e "Check with the Solus Team to see if they warn against updating Unstable"
+	echo -e "From time to time they say \" Don't Update if your on Unstable until futher notice \"  "
+	echo
+	echo -e "PLEASE NOTE: this does NOT update/upgrade anything... IT JUST ADDS/ENABLES the Unstable repo."
+	userPrompt
 
 }
 
-function addUnstableRepo
+# This function is for people who HAVE enabled the Unstable repo and want to switch back to the Stable branch
+function enableStableRepo
 {
-   sudo eopkg add-repo Unstable https://packages.solus-project.com/unstable/eopkg-index.xml.xz
-	 clear
-	 echo -e "Unstable Solus repo added and enabled"
-	 echo -e "*** IMPORTANT **** "
-	 echo -e "Check with the Solus Team to see if they warn against updating Unstable"
-	 echo -e "From time to time they say \" Don't Update if your on Unstable until futher notice \"  "
-	 userPrompt
+   	sudo eopkg disable-repo SolusUnstable 
 
-}
+   	echo -e "About to disable the Unstable branch (if you added/enabled it) and switching back to the default Solus Stable branch"
+   	userPrompt
+ 	sudo eopkg enable-repo Stable
 
-function disableRepo
-{
-	sudo eopkg disable-repo $1
-}
+ 	# Note: this is all useless I supposeo if someone clicked this option by mistake and never added the unstable branch 
 
-function enableRepo
-{
-	sudo eopkg enable-repo $1
 }
 
 
@@ -42,17 +49,15 @@ x=1
 while [[ $x=1 ]]; do
 
 	clear
-	echo -e "Xmetal's Solus Script \n"
-	echo -e "Please Select your Choice \n"
+	echo -e 	"Xmetal's Solus Script \n"
+	echo -e 	"Please Select your Choice \n"
 
-	echo -e 	"1.	\t Switch from Stable to Unstable Repos"
-	echo -e 	"2. \t Switch from Unstable to Stable Repos"
+	echo -e 	"1.	Switch from Stable to Unstable Repos"
+	echo -e 	"2. \tSwitch from Unstable to Stable Repos"
 
-	echo -e 	"Individual Tasks below"
-	echo -e   	"3. \t Disable Stable Repo"
-	echo -e   	"4. \t Add UNStable Repo"
-  	echo -e   	"5. \t Enable UNStable Repo (you must have it added first)"
-	echo -e   	"6. \t Disable UNStable Repo (useful for when the Solus Team says \"Dont Update Unstable, now\")"
+	echo -e 	"\nIndividual Tasks below"
+	echo -e   	"3. \tAdd Unstable Repo (DOES NOT disable the default repo or ENABLE Unstable repos"
+	echo -e   	"4. \tDisable UNStable Repo (useful for when the Solus Team says \"Dont Update, if your on the Unstable, now\")"
 
 	echo -e 	"m. \t Return to Main Menu"
 	echo -e 	"q.	 Exit to Prompt \n\n"
@@ -62,24 +67,17 @@ while [[ $x=1 ]]; do
 	case $fedoraSelection in
 
 
-		1) ## Adds Unstable repo and disables stable in one shot
-			addUnstableRepo
-			disableRepo Solus ;;
+		1) enableUnstableRepo 
+			x=1 ;;
 
-		2)	disableRepo Unstable
-			enableRepo Stable ;;
+		2) enableStableRepo
+			x=1 ;;
 
-    	3) disableRepo Solus
-	    	x=1 ;;
+	  	3) sudo eopkg add-repo SolusUnstable $solusUnstableRepo
+	       x=1 ;;
 
-	  	4) addUnstableRepo
-	     	x=1 ;;
-
-		5) enableRepo Unstable
+		4) sudo eopkg disable-repo solusUnstableRepo
 		   x=1	;;
-
-    	6) disableRepo Unstable
-			 x=1 ;;
 
 		[mM])	clear 
 				exit
@@ -89,7 +87,5 @@ while [[ $x=1 ]]; do
 
 		* ) invalidSection ;;
 
-
 		esac
-
 done
