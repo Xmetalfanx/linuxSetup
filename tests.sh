136b71ef5dd42a2b4ad741468f8e5a43873fc180
Xmetalfanx <Xmetalfanx@yahoo.com>
2018-02-21 15:24:51 -0500

    Starting work on "Universal install Useful Apps" code
    - Issue: it thinks the list of apps are functions, atm

--- tests.sh
+++ tests.sh
@@ -1,17 +1,29 @@
+universalFunctions= . $functionsdir/includeUniversalFunctions.cfg
+$universalFunctions
+
+###################################################################
+
+
 while [ $x=1 ]; do
 
-    clear 
+     
     echo -e   "1. \t Test - Fedora test if a pacakge is installed"
     echo -e   "2. \t Test - get Plasma info"
+    echo -e   "3. \t Test - Univeral Install usefulApps function"
 
     echo -e   "q. \t Exit to Prompt \n\n"
     read -p   "Your Choice?:" testsSelection
 
     case $testsSelection in
 
-      1) dnfCheckPkgInstalled hexchat ;;
+      1) dnfCheckPkgInstalled hexchat 
+          x=1 ;; 
 
-      2) getPlasmaInfo $plasmaDEVersion $plasmaFrameworkVersion;;
+      2) getPlasmaInfo $plasmaDEVersion $plasmaFrameworkVersion 
+          x=1 ;; 
+
+      3) massInstallUsefulApps 
+         x=1 ;; 
 
       [qQ]) quitScript  ;;
 
