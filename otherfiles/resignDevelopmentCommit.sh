8c279c1e63133c6b906c8a4cb838bf9ab4e95d12
Xmetalfanx <Xmetalfanx@yahoo.com>
2018-04-13 16:44:42 -0400

    Added a few Codacy.com recommendations

--- otherfiles/resignDevelopmentCommit.sh
+++ otherfiles/resignDevelopmentCommit.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ## First try at signing .. this i will leave here but may be 100% wrong, code-wise
 #git filter-branch --commit-filter 'git commit-tree -S "$@";' -- --all
 
--- test/awktest.sh
+++ test/awktest.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 awk -F "=" '{ print $2 }' /etc/os-release | sed 's/\"//g'
