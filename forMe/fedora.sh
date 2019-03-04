1e5f8a41419f81ae440d39f903620674b5205dd3
Xmetalfanx <Xmetalfanx@yahoo.com>
2019-03-04 15:39:13 -0500

    Fedora and OpenSuse ForMe Script work

--- forMe/fedora.sh
+++ forMe/fedora.sh
@@ -1,68 +1,135 @@
 #!/bin/bash
 
+
+#################################################
+## Variables 
+prompt="read -p 'Press [Enter] to continue' "
+rpmInstall="sudo rpm -i"
+dnfInstall="sudo dnf install"
+vscodeURL="https://go.microsoft.com/fwlink/?LinkID=760867"
+software="filezilla picard easytag deluge hexchat firefox deadbeef mpv smplayer"
+
+#################################################
+
+
+#################################################
+# Functions 
+
+function prompB4proceeding
+{
+    $prompt
+    
+    # Clears Screen before next task 
+    clear
+}
+
+function distUpgrade 
+{
+    sudo dnf upgrade -y 
+    echo "Fedora Upgrade Complete"
+    prompB4proceeding
+
+}
+
+function addRPMFusion
+{
+    echo "Adding BOTH RPMFusion Free and Non-Free Repos"
+
+    echo -e "Setting up RPM Fusion repos"
+    $dnfInstall https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+    echo "RPM Fusion Repos Added"
+}
+
+function massFedyTasks
+{
+    # Add RPM Fusion Repos
+
+
+    # Setup Codecs/Multimedia 
+        echo -e "Preparing Codecs"
+        sudo dnf -y groupinstall multimedia
+
+        # why this too? ... eh well ... leave it for now 
+        $dnfInstall gstreamer1-libav
+        echo -e "Codecs are now installed"
+
+    #######################################################
+
+    # Archive Support
+        echo "Installing Archive Support"
+        $dnfInstall cabextract lzip p7zip p7zip-plugins unrar
+
+    ########################################################
+
+    # Better Font Rendering 
+        echo "Improve Font Rendoring"
+        installApp fedy-font-config
+
+        gsettings set org.gnome.settings-daemon.plugins.xsettings antialiasing "rgba"
+        gsettings set org.gnome.settings-daemon.plugins.xsettings hinting "slight"
+
+        echo "Font Rendering Tweaks Done"
+
+}
+
+function installTheming
+{
+
+    ########################################################
+
+    # 7 - Theme Engines 
+    $dnfInstall gtk-murrine-engine gtk-unico-engine
+    ########################################################
+
+    mintTheming="mint-themes mint-y-icons mint-x-icons"
+    numixTheming="numix-gtk-theme numix-icon-theme numix-icon-theme-circle"
+
+
+    # 8 - Theme Installs 
+    $dnfInstall adapta-gtk-theme $numixTheming arc-theme bluebird-gtk3-theme clearlooks-phenix-gtk3-theme greybird-gtk3-theme bluebird-gtk2-theme greybird-gtk2-theme adwaita-gtk2-theme clearlooks-phenix-gtk2-theme $mintTheming
+    ########################################################
+
+}
+
+
+function installVSCodeRPM
+{
+
+# 3 - Install vscode via RPM 
+    echo "Installing vscode via RPM "
+    wget -o vscode.rpm $vscodeURL
+    $rpmInstall vscode.rpm
+
+    echo "vscode Installed"
+    prompB4proceeding
+
+}
+
+## Software I use 
+function installCommonSoftware
+{
+    # Install software 
+    sudo dnf install $software
+
+    installVSCodeRPM
+
+}
+# End Functions 
+################################################
+
+
 # 1 - Make updates faster 
 echo "fastestmirror=true" >> /etc/dnf/dnf.conf
 ########################################################
 
 # 2 - Upgrade system
-sudo dnf upgrade -y 
-echo "Fedora Upgrade Complete"
-########################################################
+distUpgrade
 
+# 3 - Fedy Tasks 
+massFedyTasks
 
-# 3 - Add RPM Fusion Repos 
-echo "Adding BOTH RPMFusion Free and Non-Free Repos"
+# 4 - Install Software 
+installCommonSoftware
 
-echo -e "Setting up RPM Fusion repos"
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
-echo "RPM Fusion Repos Added"
-########################################################
-
-
-
-
-# 4 - Setup Codecs/Multimedia 
-echo -e "Preparing Codecs"
-sudo dnf -y groupinstall multimedia
-
-# why this too? ... eh well ... leave it for now 
-sudo dnf install gstreamer1-libav
-echo -e "Codecs are now installed"
-########################################################
-
-
-
-# 5 - Archive Support
-echo "Installing Archive Support"
-installApp cabextract lzip p7zip p7zip-plugins unrar
-
-########################################################
-
-
-
-
-# 6 - Better Font Rendering 
-echo "Improve Font Rendoring"
-installApp fedy-font-config
-
-gsettings set org.gnome.settings-daemon.plugins.xsettings antialiasing "rgba"
-gsettings set org.gnome.settings-daemon.plugins.xsettings hinting "slight"
-
-echo "Font Rendering Tweaks Done"
-
-########################################################
-
-# 7 - Theme Engines 
-sudo dnf install gtk-murrine-engine gtk-unico-engine
-########################################################
-
-
-# 8 - Theme Installs 
-sudo dnf install adapta-gtk-theme numix-gtk-theme numix-icon-theme numix-icon-theme-circle arc-theme bluebird-gtk3-theme clearlooks-phenix-gtk3-theme greybird-gtk3-theme bluebird-gtk2-theme greybird-gtk2-theme adwaita-gtk2-theme clearlooks-phenix-gtk2-theme
-########################################################
-
-
-# 9 - Install Apps 
-sudo dnf install filezilla picard easytag deluge hexchat firefox deadbeef mpv smplayer 
-########################################################
+# 5 - Install Theming 
