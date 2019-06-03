1aabd7c469318df48a8d0140b27ae9a66a16695b
Xmetalfanx <Xmetalfanx@yahoo.com>
2019-06-02 23:55:34 -0400

    Menu adjustments - Edited/Fixed return to prior menu option, exiting scripts

--- linuxSetupScripts.sh
+++ linuxSetupScripts.sh
@@ -22,61 +22,71 @@ x=1
 # in f_common.cfg 
 intialTasks
 
-while [ $x=1 ]; do
-
-    #paddingBorder  
-
-    echo -e "${bold}${underline}Xmetal's Linux Scripts${completeReset} \n"
-
-    echo -e   "1. \t Update your System"
-    echo -e   "2. \t Optimizing Mirrors/Repos"
-    echo -e   "3. \t Arch-based"
-    echo -e   "4. \t Fedora"
-    
-    # Commented out just for now since there is no point to this menus .. the numbers stay the same 
-    #echo -e   "5. \t OpenSUSE"
-    
-    echo -e   "6. \t Solus"
-    echo -e   "7. \t Ubuntu based"
-    
-
-    # only for a space 
-    echo
-    echo -e   "C. \t Install Third Party Codecs"
-    echo -e   "I. \t Display info detected by the scripts"
-    echo -e   "R. \t Install ThirdParty Repos"
-    echo -e   "S. \t Install Software"
-    echo -e   "T. \t Theming"
-
-    echo -e   "q. \t Exit to Prompt \n\n"
-    read -p   "Your Choice?:" mainSelection
-
-    case $mainSelection in
-
-      1) universalUpdate ;;
-      2) optimizeRepo ;; 
-
-      # Located in /tasks/functions/f_mainMenus.cfg
-      3) mainArchMenu ;;
-      4) mainFedoraMenu  ;;
-      5) mainOpenSUSEMenu  ;;
-      6) mainSolusMenu ;;
-      7) mainUbuntuMenu ;; 
 
 
-      [cC]) universalCodecInstall ;; 
+function mainMenu()
+{
 
-      [iI]) massInfoOutput ;; 
+    while [ $x=1 ]; do
 
-      [rR]) thirdPartyRepoCheck ;; 
+        #paddingBorder  
+
+        echo -e "${bold}${underline}Xmetal's Linux Scripts${completeReset} \n"
+
+        echo -e   "1. \t Update your System"
+        echo -e   "2. \t Optimizing Mirrors/Repos"
+        echo -e   "3. \t Arch-based"
+        echo -e   "4. \t Fedora"
+        
+        # Commented out just for now since there is no point to this menus .. the numbers stay the same 
+        #echo -e   "5. \t OpenSUSE"
+        
+        echo -e   "6. \t Solus"
+        echo -e   "7. \t Ubuntu based"
+        
+
+        # only for a space 
+        echo
+        echo -e   "C. \t Install Third Party Codecs"
+        echo -e   "I. \t Display info detected by the scripts"
+        echo -e   "R. \t Install ThirdParty Repos"
+        echo -e   "S. \t Install Software"
+        echo -e   "T. \t Theming"
+
+        echo -e   "q. \t Exit to Prompt \n\n"
+        read -p   "Your Choice?:" mainSelection
+
+        case $mainSelection in
+
+          1) universalUpdate ;;
+          2) optimizeRepo ;; 
+
+          # Located in /tasks/functions/f_mainMenus.cfg
+          3) mainArchMenu ;;
+          4) mainFedoraMenu  ;;
+          5) mainOpenSUSEMenu  ;;
+          6) mainSolusMenu ;;
+          7) mainUbuntuMenu ;; 
+
+
+          [cC]) universalCodecInstall ;; 
+
+          [iI]) massInfoOutput ;; 
+
+          [rR]) thirdPartyRepoCheck ;; 
+          
+          [sS]) universalSoftwareMenu ;;
+
+          [tT]) universalThemingMenu ;; 
       
-      [sS]) universalSoftwareMenu ;;
+          [qQ]) quitScript  ;;
 
-      [tT]) universalThemingMenu ;; 
-  
-      [qQ]) quitScript  ;;
+          * ) invalidSection ;;
 
-      * ) invalidSection ;;
+          esac
+    done
 
-      esac
-done
+}
+
+# Call the main menu 
+mainMenu
