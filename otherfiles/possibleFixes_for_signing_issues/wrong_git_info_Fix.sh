0644617b7d1b65a72456d01783a06702ae7e3cd1
Xmetalfanx <Xmetalfanx@yahoo.com>
2020-06-07 13:25:06 -0400

    PPA code updates

---
+++ otherfiles/possibleFixes_for_signing_issues/wrong_git_info_Fix.sh
@@ -0,0 +1,6 @@
+git filter-branch -f --env-filter '
+    GIT_AUTHOR_NAME="Xmetalfanx"
+    GIT_AUTHOR_EMAIL="Xmetalfanx@yahoo.com"
+    GIT_COMMITTER_NAME="Xmetalfanx"
+    GIT_COMMITTER_EMAIL="Xmetalfanx@yahoo.com"
+  ' HEAD
