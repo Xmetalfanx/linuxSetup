abaf6636097f4961bd3d1c7f4bb11386f6a17bae
Xmetalfanx <Xmetalfanx@yahoo.com>
2022-01-09 14:26:24 -0500

    added gitsetup.sh file

---
+++ gitsetup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git config --global user.email "Xmetalfanx@yahoo.com"
+git config --global user.name "Xmetalfanx"
+
+#git config --global user.signingkey #keyHere
+
+# commented out as I do not know if i will have/have to setup gpg yet 
+#git config --global commit.gpgsign true
